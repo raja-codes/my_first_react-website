569b75bcdc9d722408b7419275a8aa583a716f44
rajadevpy <rajathe112233@gmail.com>
2021-03-04 15:14:20 +0530

    First React Website

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../../App.css'
-import { Button } from '../Button'
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
